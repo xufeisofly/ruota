86bc898643452796ae40c2c38f79a6915a0a7f3f
Norris <feixu@tju.edu.cn>
2019-02-18 22:24:26 +0800

    socket

--- client_socket.go
+++ client_socket.go
@@ -1,1 +1,30 @@
 package ruota
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+type RClientSocket struct {
+	Addr    net.Addr
+	Conn    net.Conn
+	Timeout time.Duration
+}
+
+const DEFAULT_TIMEOUT = 10 * time.Second
+
+func NewRClientSocket(host, port string) (*RClientSocket, error) {
+	addr, err := net.ResolveTCPAddr("tcp", net.JoinHostPort(host, port))
+	return &RClientSocket{Addr: addr, Timeout: DEFAULT_TIMEOUT}, err
+}
+
+func (p *RClientSocket) Dial() error {
+	conn, err := net.Dial("tcp", p.Addr.String())
+	if err != nil {
+		return err
+	}
+	p.Conn = conn
+	fmt.Println("Client Socket Dialed")
+	return err
+}
--- server_socket.go
+++ server_socket.go
@@ -1,1 +1,39 @@
 package ruota
+
+import (
+	"net"
+	"time"
+)
+
+type RServerSocket struct {
+	Addr     net.Addr
+	Conn     net.Conn
+	Timeout  time.Duration
+	Listener net.Listener
+}
+
+const DEFAULT_SERVER_TIMEOUT = 10 * time.Second
+
+func NewRServerSocket(host, port string) (*RServerSocket, error) {
+	addr, err := net.ResolveTCPAddr("tcp", net.JoinHostPort(host, port))
+	return &RServerSocket{Addr: addr, Timeout: DEFAULT_SERVER_TIMEOUT}, err
+}
+
+func (p *RServerSocket) Listen() error {
+	l, err := net.Listen("tcp", p.Addr.String())
+	if err != nil {
+		return err
+	}
+	// Save listener to RServerSocket
+	p.Listener = l
+	return nil
+}
+
+func (p *RServerSocket) Accept() error {
+	conn, err := p.Listener.Accept()
+	if err != nil {
+		return err
+	}
+	p.Conn = conn
+	return nil
+}
